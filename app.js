073a9f23b7e7f21e5903b479fd8cbc0ab9e79649
kadjialan <alankadji7@gmail.com>
2023-10-12 12:43:55 +0100

    backend setup

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const dotenv= require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 dotenv.config()
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/',(req, res) => res.send('server is ready'))
-
-app.listen(port, () => console.log(`server started on ${port}`))
+app.get('/', (req, res) =>res.send('server is ready'));
+app.listen(port, () => console.log(`server started on port ${port}`));
